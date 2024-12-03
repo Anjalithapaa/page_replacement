a08d60fb82e9a684276ecfb3dd870ca8c0d73d73
Anjalithapaa <anjali.thapa@selu.edu>
2024-12-02 21:32:13 -0600

    updated code

--- main.cpp
+++ main.cpp
@@ -1,125 +1,285 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <algorithm>
-#include <fstream>
+#include <cstdio>
+#include <limits>
+#include <climits>
 
-// Function to load addresses from the file
-std::vector<int> loadAddresses(const std::string &filename)
+// Page Replacement Simulation Program
+// Implements three page replacement algorithms:
+// 1. FIFO (First-In-First-Out)
+// 2. LRU (Least Recently Used)
+// 3. Optimal (Belady's Algorithm)
+
+// Constant definitions
+#define PAGE_SIZE 100 // Size of each page
+#define MAX_FRAMES 5  // Number of memory frames available
+
+// Algorithm mode identifiers
+#define FIFO 1 // First-In-First-Out replacement
+#define LRU 2  // Least Recently Used replacement
+#define OPT 3  // Optimal replacement algorithm
+
+// Structure to represent page data in memory frames
+typedef struct
 {
-    std::vector<int> addresses;
-    std::ifstream file(filename); // Open the file for reading
-    int address;
-    while (file >> address)
-    {                                 // Read each address from the file
-        addresses.push_back(address); // Store the address in the vector
-    }
-    return addresses;
-}
+    int pageNum;   // Page number stored in this frame
+    int usage;     // Number of times page has been used
+    int lastUsed;  // Index when page was last accessed
+    int timeStamp; // Time of page insertion into frame
+} pageData;
 
-// FIFO (First In First Out) Page Replacement Algorithm
-int fifo(const std::vector<int> &addresses, int frameCount)
-{
-    std::queue<int> frames;                 // Queue to maintain the order of pages in memory
-    std::unordered_map<int, bool> inMemory; // Map to track which pages are currently in memory
-    int pageFaults = 0;                     // Counter for the number of page faults
+// Global arrays to store address stream and memory frames
+int adds[100];               // Array to store memory address stream
+pageData frames[MAX_FRAMES]; // Array representing memory frames
 
-    for (int address : addresses)
-    { // Loop through each address in the input
-        if (!inMemory[address])
-        { // Check if the page is not in memory
-            pageFaults++;
-            if (frames.size() == frameCount)
-            {                                 // If all frames are full
-                int toEvict = frames.front(); // Identify the oldest page
-                frames.pop();                 // Remove it from the queue
-                inMemory.erase(toEvict);      // Mark it as no longer in memory
-            }
-            frames.push(address);     // Add the new page to the memory queue
-            inMemory[address] = true; // Mark it as in memory
-        }
-    }
-    return pageFaults;
-}
+// Function prototypes
+int readAddressStream(const char *filename);    // Read addresses from input file
+void showAdds(int numAdds);                     // Display address stream
+int pageReplace(int numAdds, char mode);        // Main page replacement simulation
+int searchFrameTable(int pageNum, int nFrames); // Check if page exists in frames
+void showFrameTable(int nFrames);               // Display current frame contents
 
-// LRU (Least Recently Used) Page Replacement Algorithm
-int lru(const std::vector<int> &addresses, int frameCount)
-{
-    std::unordered_map<int, int> lastUsed; // Map to track the last used index of each page
-    std::vector<int> frames;               // Vector to store pages in memory
-    int pageFaults = 0;                    // Counter for the number of page faults
-
-    for (int i = 0; i < addresses.size(); i++)
-    { // Loop through each address
-        int address = addresses[i];
-        if (std::find(frames.begin(), frames.end(), address) == frames.end())
-        {
-            // If the page is not in memory
-            pageFaults++; // Increment page fault count
-            if (frames.size() == frameCount)
-            {                     // If memory is full
-                int lruIndex = 0; // Initialize index of the least recently used page
-                for (int j = 1; j < frames.size(); j++)
-                { // Find the LRU page
-                    if (lastUsed[frames[j]] < lastUsed[frames[lruIndex]])
-                    {
-                        lruIndex = j; // Update index of the least recently used page
-                    }
-                }
-                frames.erase(frames.begin() + lruIndex); // Remove the LRU page from memory
-            }
-            frames.push_back(address); // Add the new page to memory
-        }
-        lastUsed[address] = i; // Update the last used index for the page
-    }
-    return pageFaults;
-}
-
-// Function to analyze Belady's Anomaly
-void beladyAnalysis(const std::vector<int> &addresses)
-{
-    std::cout << "\nBelady's Anomaly Analysis:\n";
-    for (int frames = 1; frames <= 10; frames++)
-    {                                         // Loop through different frame sizes
-        int faults = fifo(addresses, frames); // Use FIFO to calculate page faults
-        std::cout << "Frames: " << frames << ", Page Faults: " << faults << std::endl;
-    }
-}
+// Replacement strategy selector functions
+int getIndexOfOldestPage(int nFrames);                   // FIFO: Find oldest page in frames
+int getIndexOfLRUPage(int nFrames);                      // LRU: Find least recently used page
+int getIndexOfBeladyPage(int nFrames, int currentIndex); // Optimal: Find page to replace
 
 int main()
 {
-    // Load addresses from the input file
-    std::string filename = "address.txt";
-    std::vector<int> addresses = loadAddresses(filename); // Load addresses into a vector
+    int numAdds;    // Total number of addresses
+    int pageFaults; // Page fault counter
 
-    if (addresses.empty())
-    { // Check if the file was empty or not found
-        std::cerr << "Error: Address file is empty or not found.\n";
-        return 1;
-    }
+    // User greeting
+    printf("YO HO HO !\n");
 
-    // Ask the user for the number of frames
-    int frameCount;
-    std::cout << "Enter the number of frames: ";
-    std::cin >> frameCount;
+    // Read address stream from file
+    const char *filename = "address.txt";
+    numAdds = readAddressStream(filename);
+    printf("Total Addresses Read: %d \n", numAdds);
 
-    if (frameCount <= 0)
-    { // Validate the frame count
-        std::cerr << "Error: Frame count must be greater than 0.\n";
-        return 1;
-    }
+    // Display input address stream
+    showAdds(numAdds);
 
-    // Run FIFO algorithm
-    int fifoFaults = fifo(addresses, frameCount);
-    std::cout << "\nFIFO Page Faults: " << fifoFaults << std::endl;
+    // Simulate FIFO Page Replacement
+    printf("\n--- FIFO Page Replacement ---\n");
+    pageFaults = pageReplace(numAdds, FIFO);
+    printf("FIFO Page Faults: %d \n", pageFaults);
 
-    // Run LRU algorithm
-    int lruFaults = lru(addresses, frameCount);
-    std::cout << "LRU Page Faults: " << lruFaults << std::endl;
+    // Simulate LRU Page Replacement
+    printf("\n--- LRU Page Replacement ---\n");
+    pageFaults = pageReplace(numAdds, LRU);
+    printf("LRU Page Faults: %d \n", pageFaults);
 
-    // Perform Belady's Analysis
-    beladyAnalysis(addresses);
+    // Simulate Optimal Page Replacement
+    printf("\n--- Optimal Page Replacement ---\n");
+    pageFaults = pageReplace(numAdds, OPT);
+    printf("Optimal Page Faults: %d \n", pageFaults);
 
     return 0;
+}
+
+// Read memory addresses from input file
+int readAddressStream(const char *filename)
+{
+    FILE *in;
+    int address;
+    int j = 0;
+
+    // Open file with error checking
+    in = fopen(filename, "r");
+    if (in == NULL)
+    {
+        printf("Error: Cannot open file %s\n", filename);
+        return 0;
+    }
+
+    // Read addresses into global array
+    while (fscanf(in, "%d", &address) != EOF)
+    {
+        adds[j++] = address;
+    }
+    fclose(in);
+    return j;
+}
+
+// Display all memory addresses read from input file
+void showAdds(int numAdds)
+{
+    printf("Address Stream: \n");
+    for (int j = 0; j < numAdds; j++)
+    {
+        printf("%d \n", adds[j]);
+    }
+}
+
+// Search if page exists in memory frames
+int searchFrameTable(int pageNum, int nFrames)
+{
+    // Linear search through frames
+    for (int j = 0; j < nFrames; j++)
+    {
+        if (frames[j].pageNum == pageNum)
+        {
+            return j; // Page found, return frame index
+        }
+    }
+    return -1; // Page not found
+}
+
+// FIFO: Find frame with oldest timestamp
+int getIndexOfOldestPage(int nFrames)
+{
+    int oldIndex = 0;
+    for (int j = 1; j < nFrames; j++)
+    {
+        // Compare timestamps to find earliest inserted page
+        if (frames[j].timeStamp < frames[oldIndex].timeStamp)
+        {
+            oldIndex = j;
+        }
+    }
+    return oldIndex;
+}
+
+// LRU: Find page with least usage
+int getIndexOfLRUPage(int nFrames)
+{
+    int leastRU = frames[0].usage;
+    int lIndex = 0;
+
+    // Find page with minimum usage count
+    for (int j = 1; j < nFrames; j++)
+    {
+        if (frames[j].usage < leastRU)
+        {
+            leastRU = frames[j].usage;
+            lIndex = j;
+        }
+    }
+    return lIndex;
+}
+
+// Optimal (Belady's) Algorithm: Predict future page usage
+int getIndexOfBeladyPage(int nFrames, int currentIndex)
+{
+    int maxFutureDistance = -1;
+    int replaceIndex = 0;
+
+    // For each frame in memory
+    for (int j = 0; j < nFrames; j++)
+    {
+        int futureDistance = INT_MAX;
+
+        // Look ahead to find next occurrence of this page
+        for (int k = currentIndex + 1; k < 100; k++)
+        {
+            if (adds[k] / PAGE_SIZE == frames[j].pageNum)
+            {
+                futureDistance = k - currentIndex;
+                break;
+            }
+        }
+
+        // If page won't be used again, immediately replace it
+        if (futureDistance == INT_MAX)
+        {
+            return j;
+        }
+
+        // Find page that will be used furthest in the future
+        if (futureDistance > maxFutureDistance)
+        {
+            maxFutureDistance = futureDistance;
+            replaceIndex = j;
+        }
+    }
+
+    return replaceIndex;
+}
+
+// Main page replacement simulation
+int pageReplace(int numAdds, char mode)
+{
+    int nFrames = 0;     // Current number of frames in use
+    int pageFaults = 0;  // Counter for page faults
+    int timeCounter = 0; // Global timestamp for page insertion
+
+    // Process each memory address
+    for (int j = 0; j < numAdds; j++)
+    {
+        // Calculate page number and offset
+        int pageNum = adds[j] / PAGE_SIZE;
+        int offset = adds[j] % PAGE_SIZE;
+
+        // Check if page already in memory
+        int frameNum = searchFrameTable(pageNum, nFrames);
+
+        // Page not in memory (Page Fault)
+        if (frameNum == -1)
+        {
+            // Room available in frames
+            if (nFrames < MAX_FRAMES)
+            {
+                frames[nFrames].pageNum = pageNum;
+                frames[nFrames].usage = 1;
+                frames[nFrames].lastUsed = j;
+                frames[nFrames].timeStamp = timeCounter++;
+                nFrames++;
+                pageFaults++;
+            }
+            // No room, need page replacement
+            else
+            {
+                // Select replacement strategy
+                int repFrame;
+                switch (mode)
+                {
+                case FIFO:
+                    repFrame = getIndexOfOldestPage(MAX_FRAMES);
+                    break;
+                case LRU:
+                    repFrame = getIndexOfLRUPage(MAX_FRAMES);
+                    break;
+                case OPT:
+                    repFrame = getIndexOfBeladyPage(MAX_FRAMES, j);
+                    break;
+                }
+
+                // Replace selected frame
+                frames[repFrame].pageNum = pageNum;
+                frames[repFrame].usage = 1;
+                frames[repFrame].lastUsed = j;
+                frames[repFrame].timeStamp = timeCounter++;
+
+                pageFaults++;
+            }
+        }
+        // Page already in memory
+        else
+        {
+            // Update usage statistics
+            frames[frameNum].usage++;
+            frames[frameNum].lastUsed = j;
+        }
+
+        // Display current frame state
+        showFrameTable(nFrames);
+    }
+    return pageFaults;
+}
+
+// Display current memory frame contents
+void showFrameTable(int nFrames)
+{
+    printf("Frame Table - ");
+    for (int j = 0; j < MAX_FRAMES; j++)
+    {
+        if (j < nFrames)
+        {
+            printf("%d ", frames[j].pageNum);
+        }
+        else
+        {
+            printf("# "); // Unused frames
+        }
+    }
+    printf("\n");
 }
